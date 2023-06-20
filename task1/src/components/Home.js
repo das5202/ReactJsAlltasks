b8a99e6571c5279a1badd2a08dcaf9e44c8918e1
Rani Dasari <devika.rani-dasari@capgemini.com>
2023-06-20 21:28:01 +0530

    task 1 completed

--- task1/src/components/Home.js
+++ task1/src/components/Home.js
@@ -1,12 +1,16 @@
-import React, { Fragment } from 'react';
+import React, { Fragment} from 'react';
 import {Button,Table} from 'react-bootstrap';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Employees from './Employees';
 import {Link,useNavigate} from 'react-router-dom';
 
 
-function Home(){
+
+function Home()
+{
+    
     let history = useNavigate();
+    
     const handleDelete=(Id)=>{
         var index=Employees.map(function(e){
             return e.Id
@@ -15,21 +19,32 @@ function Home(){
         history('/');
 
     }
+    const handleClear=(item)=>{
+     /* var index = Employees.map(function(f){
+        return null
+      });*/
+      Employees.splice(0);
+      
+     
+      history('/');
+    }
     return(
         <Fragment>
             <div style={{margin:"10rem"}}>
-               
-                <Table stripped bordered hover size="sm">
-                    <thead>
-                        <te>
+                <center><h3>Employee List </h3> </center>
+       <Table className =" table text-center" >
+                   <thead>
+                        <tr className="table-dark">
                             <th >
                                 Name
                             </th>
-
                             <th>
                                Image
                             </th>
-                        </te>
+                            <th>
+
+                            </th>
+                            </tr>
                     </thead>
                     <tbody>
                         {
@@ -47,20 +62,27 @@ function Home(){
                                            <img src= {item.Image} width="100" height="100"/>
                                         </td>
                                         <td>
-                                            <button onClick={()=>handleDelete(item.Id)}>DELETE</button>
-                                        </td>
+                                            <button className="btn btn-outline-danger" onClick={()=>handleDelete(item.Id)}>DELETE</button>
+                                        </td>                      
                                     </tr>
                                 )
                             })
                             :
                             "No avaliable data to display"
-
                         }
                     </tbody>
                 </Table>
+                
+                <div>                        
+                <button className="btn btn-outline-primary" onClick={()=>handleClear()}>ClearAll</button>
+                </div>
+                
+                <br></br>
+                <br></br>
             </div>
         </Fragment>
     )
 }
+
 export default Home;
     
